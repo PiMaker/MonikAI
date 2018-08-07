9b31c0cf5f957b4cb5386bf653cefde9fabfed5a
PiMaker <pi@pimaker.at>
2018-08-07 22:12:15 +0200

    Fix for .NET version

--- MonikAI/MonikaiSettings.Designer.cs
+++ MonikAI/MonikaiSettings.Designer.cs
@@ -12,7 +12,7 @@ namespace MonikAI {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.6.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.8.0.0")]
     internal sealed partial class MonikaiSettings : global::System.Configuration.ApplicationSettingsBase {
         
         private static MonikaiSettings defaultInstance = ((MonikaiSettings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new MonikaiSettings())));
